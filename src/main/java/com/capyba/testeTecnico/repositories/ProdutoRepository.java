027e3c3d7ae1707533e912de9a33e093dfdbeab2
JoaoTorpe <victotorpe10@gmail.com>
2023-11-15 14:35:19 -0300

    Consulta para filtrar por disponibilidade do produto

--- src/main/java/com/capyba/testeTecnico/repositories/ProdutoRepository.java
+++ src/main/java/com/capyba/testeTecnico/repositories/ProdutoRepository.java
@@ -1,6 +1,10 @@
 package com.capyba.testeTecnico.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.capyba.testeTecnico.entities.Produto;
@@ -8,6 +12,6 @@ import com.capyba.testeTecnico.entities.Produto;
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
-	
-	
+	@Query("SELECT p FROM Produto p WHERE p.disponivel = :disponivel")
+    List<Produto> findByDisponibilidade(@Param("disponivel") boolean disponivel);
 }
