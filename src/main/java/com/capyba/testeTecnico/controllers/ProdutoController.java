32e5706b23dbdb841eb49391639d1f9bae92eabd
JoaoTorpe <victotorpe10@gmail.com>
2023-11-15 14:34:33 -0300

    Endpoint para filtrar por disponibilidade do produto

--- src/main/java/com/capyba/testeTecnico/controllers/ProdutoController.java
+++ src/main/java/com/capyba/testeTecnico/controllers/ProdutoController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.capyba.testeTecnico.entities.Produto;
 import com.capyba.testeTecnico.repositories.ProdutoRepository;
 
+import jakarta.websocket.server.PathParam;
+
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
@@ -20,17 +22,24 @@ public class ProdutoController {
 
 	@GetMapping
 	public ResponseEntity<List<Produto>> retornaTodos() {
-		
-	
 		List<Produto> produtos =  repository.findAll();
 		
 		if(produtos.isEmpty()) {
 			return ResponseEntity.badRequest().build();
 		}
-		
 		return ResponseEntity.ok().body(produtos);
+	}
+	
+	@GetMapping(params = "disponivel")
+	public ResponseEntity<List<Produto>> estaDisponivel(@PathParam("disponivel") boolean disponivel){
+		
+		List<Produto> produtosFiltrados = repository.findByDisponibilidade(disponivel);
+		
+		return ResponseEntity.ok().body(produtosFiltrados);
 		
 	}
+	
+	
 	
 	
 }
