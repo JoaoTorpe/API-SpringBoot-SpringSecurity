41233077ed125ec65452756433609da013496c65
JoaoTorpe <victotorpe10@gmail.com>
2023-11-15 15:26:24 -0300

    Consulta que ordena produtos

--- src/main/java/com/capyba/testeTecnico/repositories/ProdutoRepository.java
+++ src/main/java/com/capyba/testeTecnico/repositories/ProdutoRepository.java
@@ -2,6 +2,7 @@ package com.capyba.testeTecnico.repositories;
 
 import java.util.List;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -14,4 +15,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	
 	@Query("SELECT p FROM Produto p WHERE p.disponivel = :disponivel")
     List<Produto> findByDisponibilidade(@Param("disponivel") boolean disponivel);
+	
+	
+    List<Produto> findAll(Sort sort);
 }
