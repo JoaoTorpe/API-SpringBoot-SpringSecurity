a560bf88309fb000bd52c56b3f50f4ea47161eb9
JoaoTorpe <victotorpe10@gmail.com>
2023-11-15 15:46:21 -0300

    Consulta para buscar por nome

--- src/main/java/com/capyba/testeTecnico/repositories/ProdutoRepository.java
+++ src/main/java/com/capyba/testeTecnico/repositories/ProdutoRepository.java
@@ -16,6 +16,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 	@Query("SELECT p FROM Produto p WHERE p.disponivel = :disponivel")
     List<Produto> findByDisponibilidade(@Param("disponivel") boolean disponivel);
 	
-	
     List<Produto> findAll(Sort sort);
+    
+    List<Produto> findByNomeContaining(String nome);
 }
