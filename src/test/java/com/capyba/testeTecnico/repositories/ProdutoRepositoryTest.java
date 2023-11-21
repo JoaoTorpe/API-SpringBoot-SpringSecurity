907860aeba979db20f14e8b268e5c9d8be0cd2ec
JoaoTorpe <victotorpe10@gmail.com>
2023-11-21 09:18:08 -0300

    Teste para verificar query de ordenacao

--- src/test/java/com/capyba/testeTecnico/repositories/ProdutoRepositoryTest.java
+++ src/test/java/com/capyba/testeTecnico/repositories/ProdutoRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.capyba.testeTecnico.repositories;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.capyba.testeTecnico.entities.Produto;
@@ -33,6 +37,21 @@ class ProdutoRepositoryTest {
 		 assertThat(produtosDisponiveis).allMatch(produto -> produto.isDisponivel());
 		 assertThat(produtosNaoDisponiveis).allMatch(produto -> !produto.isDisponivel());
 	}
+	
+	@Test
+	void testaOrdenacao() {
+		generateProdutos();
+		
+		List<Produto> listaDeProdutos = Repository.findAll();
+		List<Produto> listaOrdenadaPorNome = Repository.findAll(Sort.by("nome"));
+		List<Produto> listaOrdenadaPorPreco = Repository.findAll(Sort.by("preco"));
+		
+		assertThat(listaOrdenadaPorNome).isEqualTo(listaDeProdutos.stream().sorted(Comparator.comparing(Produto::getNome)).collect(Collectors.toList()));
+		assertThat(listaOrdenadaPorPreco).isEqualTo(listaDeProdutos.stream().sorted(Comparator.comparing(Produto::getPreco)).collect(Collectors.toList()));
+		
+	}
+	
+	
 	
 	private void generateProdutos() {
 		Produto produto1 = new Produto("Produto A", true, 29.99);
